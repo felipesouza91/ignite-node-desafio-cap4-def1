0d81529c9a63736fa23d1a1ab975c3a11cd79551
Felipe S Santana <felipedb91@gmail.com>
2021-05-07 14:00:15 -0300

    test: ensure useRepository return user when id exists

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -3,7 +3,11 @@ import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUs
 import { ShowUserProfileError } from "./ShowUserProfileError";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
-
+const makeFakeUser = (): User => {
+  const user = new User();
+  user.name = 'Any name';
+  return user;
+}
 
 const makeSut = () => {
   const fakeUserRepository = new InMemoryUsersRepository();
@@ -26,8 +30,15 @@ describe('Show Use Profile Use Case', () => {
   it('ensure useRepository throws when user not exists', async () => {
     const { sut, fakeUserRepository } = makeSut();
     jest.spyOn(fakeUserRepository, 'findById').mockResolvedValueOnce(undefined);
-
     const response = sut.execute("any_id");
     expect(response).rejects.toBeInstanceOf(ShowUserProfileError);
+  });
+
+  it('ensure useRepository return user when id exists', async () => {
+    const { sut, fakeUserRepository } = makeSut();
+    jest.spyOn(fakeUserRepository, 'findById').mockResolvedValueOnce(makeFakeUser());
+    const response = await sut.execute("valid_id");
+    expect(response).toBeTruthy();
+    expect(response.id).not.toBeNull();
   });
 });
