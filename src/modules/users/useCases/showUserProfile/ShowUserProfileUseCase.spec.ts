92f47996590b9f35df1162db482fef64394ae22f
Felipe S Santana <felipedb91@gmail.com>
2021-05-07 13:57:04 -0300

    feat: ensure useRepository throws when user not exists

--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { User } from "../../entities/User";
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository";
+import { ShowUserProfileError } from "./ShowUserProfileError";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 
@@ -20,5 +21,13 @@ describe('Show Use Profile Use Case', () => {
     const repSpy = jest.spyOn(fakeUserRepository, 'findById');
     await sut.execute("any_id");
     expect(repSpy).toBeCalledWith("any_id");
+  });
+
+  it('ensure useRepository throws when user not exists', async () => {
+    const { sut, fakeUserRepository } = makeSut();
+    jest.spyOn(fakeUserRepository, 'findById').mockResolvedValueOnce(undefined);
+
+    const response = sut.execute("any_id");
+    expect(response).rejects.toBeInstanceOf(ShowUserProfileError);
   });
 });
