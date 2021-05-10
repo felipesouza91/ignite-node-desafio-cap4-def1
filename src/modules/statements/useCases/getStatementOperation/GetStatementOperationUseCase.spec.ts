e54a835c6567672c6762d7a86c18ad8b020b0624
Felipe S Santana <felipedb91@gmail.com>
2021-05-10 14:06:22 -0300

    test: ensure GetStatementOperationUseCase calls statementsRepository.findStatementOperation with correct value

--- src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -24,4 +24,15 @@ describe('Get Statement Operation Use Case', () => {
     await sut.execute({user_id: 'user_id', statement_id: "statement_id"});
     expect(repSpy).toHaveBeenCalledWith('user_id');
   });
+
+  it('ensure GetStatementOperationUseCase calls statementsRepository.findStatementOperation with correct value', async () => {
+    const { sut, userRepository, statementRepository} = makeSut();
+    jest.spyOn(userRepository, 'findById')
+      .mockResolvedValueOnce(new User());
+    jest.spyOn(statementRepository, 'findStatementOperation')
+      .mockResolvedValueOnce(new Statement());
+    const repSpy = jest.spyOn(statementRepository, 'findStatementOperation');
+    await sut.execute({user_id: 'user_id', statement_id: "statement_id"});
+    expect(repSpy).toHaveBeenCalledWith({user_id: 'user_id', statement_id:"statement_id" });
+  });
 });
