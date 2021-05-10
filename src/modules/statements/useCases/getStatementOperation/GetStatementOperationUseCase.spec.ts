1a9e954e41078b01e1a2bb554c51671e5b8a1b6e
Felipe S Santana <felipedb91@gmail.com>
2021-05-10 14:02:48 -0300

    test: ensure GetStatementOperationUseCase calls userRespository.findbyid with correct value

---
+++ src/modules/statements/useCases/getStatementOperation/GetStatementOperationUseCase.spec.ts
@@ -0,0 +1,27 @@
+import { User } from "../../../users/entities/User";
+import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
+import { Statement } from "../../entities/Statement";
+import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
+import { GetStatementOperationUseCase } from "./GetStatementOperationUseCase";
+
+const makeSut = () => {
+  const userRepository = new InMemoryUsersRepository();
+  const statementRepository = new InMemoryStatementsRepository();
+  const sut = new GetStatementOperationUseCase(userRepository, statementRepository)
+  return {
+    sut, statementRepository, userRepository
+  }
+}
+
+describe('Get Statement Operation Use Case', () => {
+  it('ensure GetStatementOperationUseCase calls userRespository.findbyid with correct value', async () => {
+    const { sut, userRepository, statementRepository} = makeSut();
+    jest.spyOn(userRepository, 'findById')
+      .mockResolvedValueOnce(new User());
+    jest.spyOn(statementRepository, 'findStatementOperation')
+      .mockResolvedValueOnce(new Statement());
+    const repSpy = jest.spyOn(userRepository, 'findById');
+    await sut.execute({user_id: 'user_id', statement_id: "statement_id"});
+    expect(repSpy).toHaveBeenCalledWith('user_id');
+  });
+});
