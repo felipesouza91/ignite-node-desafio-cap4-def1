149a7ba5878d0a52b2fc4218e2367d73d67f8234
Felipe S Santana <felipedb91@gmail.com>
2021-05-13 14:03:26 -0300

    test: ensure return 400 when email alread is used

---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import { Connection, createConnection } from "typeorm";
+
+import {app} from '../../../../app'
+import { User } from "../../entities/User";
+
+
+let connection: Connection;
+describe('Authenticate User Controller', () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  afterEach(async () => {
+    await connection.getRepository(User).delete({});
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+  it('ensure return 400 when email alread is used', async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Admin",
+      email: "admin@finapi.com",
+      password: "password"
+    });
+    const response = await request(app).post("/api/v1/users")
+      .send({
+        name: "Admin",
+        email: "admin@finapi.com",
+        password: "password"
+      })
+      expect(response.statusCode).toBe(400);
+      expect(response.body.message).toBe("User already exists");
+  });
+});
