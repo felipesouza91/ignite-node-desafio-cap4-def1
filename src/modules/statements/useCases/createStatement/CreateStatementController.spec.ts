bb99721595251af1da1ce140cd00394338c61504
Felipe S Santana <felipedb91@gmail.com>
2021-05-14 14:10:18 -0300

    test: ensure CreateStatementController return 404 when userid not found

--- src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.spec.ts
@@ -2,6 +2,7 @@ import request from "supertest";
 import { Connection, createConnection } from "typeorm";
 
 import {app} from '../../../../app'
+import { User } from "../../../users/entities/User";
 import { Statement } from "../../entities/Statement";
 
 
@@ -20,7 +21,6 @@ describe('Authenticate User Controller', () => {
   });
 
   beforeEach( async () => {
-
     const response = await request(app).post("/api/v1/sessions").send({
       email: "admin@finapi.com",
       password: "password"
@@ -50,6 +50,25 @@ describe('Authenticate User Controller', () => {
       .send({amount: 100, description: "A deposit" })
       expect(response.statusCode).toBe(401);
       expect(response.body.message).toBe("JWT invalid token!");
+  });
+
+  it('ensure CreateStatementController return 404 when userid not found', async () => {
+    await request(app).post("/api/v1/users").send({
+      name: "Admin",
+      email: "anotheruser@finapi.com",
+      password: "password"
+    });
+    const responseToken = await request(app).post("/api/v1/sessions").send({
+      email: "anotheruser@finapi.com",
+      password: "password"
+    });
+    token = responseToken.body.token
+    await connection.getRepository(User).delete({id: responseToken.body.user.id})
+    const response = await request(app).post("/api/v1/statements/deposit")
+      .set("authorization", `Bearer ${token}`)
+      .send({amount: 100, description: "A deposit" })
+      expect(response.statusCode).toBe(404);
+      expect(response.body.message).toBe("User not found");
   });
 
   it('ensure CreateStatementController return 201 when deposit with correct values', async () => {
