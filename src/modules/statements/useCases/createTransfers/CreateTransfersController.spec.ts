c4b38067f39598c40e6fd201020793a71d57543d
Felipe Souza <felipedb91@gmail.com>
2021-05-18 21:54:07 -0300

    test: ensure CreateStatementController return 404 when userid not found

--- src/modules/statements/useCases/createTransfers/CreateTransfersController.spec.ts
+++ src/modules/statements/useCases/createTransfers/CreateTransfersController.spec.ts
@@ -7,6 +7,7 @@ import { Statement } from "../../entities/Statement";
 
 let connection: Connection;
 let token: string;
+let userId: string;
 describe("Create Transfers Controller", () => {
   beforeAll(async () => {
     connection = await createConnection();
@@ -24,6 +25,7 @@ describe("Create Transfers Controller", () => {
       password: "password",
     });
     token = response.body.token;
+    userId = response.body.user.id;
   });
 
   afterEach(async () => {
@@ -51,5 +53,20 @@ describe("Create Transfers Controller", () => {
       .send({ amount: 100, description: "A deposit" });
     expect(response.statusCode).toBe(401);
     expect(response.body.message).toBe("JWT invalid token!");
+  });
+
+  it("ensure CreateStatementController return 404 when userid not found", async () => {
+    const anotherUser = await request(app).post("/api/v1/users").send({
+      name: "Another user ",
+      email: "anotheruser@finapi.com",
+      password: "password",
+    });
+    await connection.getRepository(User).delete({ id: userId });
+    const response = await request(app)
+      .post(`/api/v1/statements/transfers/${anotherUser.body.id}`)
+      .set("authorization", `Bearer ${token}`)
+      .send({ amount: 100, description: "A deposit" });
+    expect(response.statusCode).toBe(404);
+    expect(response.body.message).toBe("User not found");
   });
 });
